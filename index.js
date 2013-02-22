39f28909571f640ee617a8a8d002c65746b6a9e5
Swaagie <info@martijnswaagman.nl>
2013-02-22 07:14:36 +0100

    added parser

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-var jsdom = require('jsdom')
-  , parser = require('htmlparser')
-  , sys = require('sys')
-  , fs = require('fs');
+var fs = require('fs')
+  , content = fs.readFileSync('index.html', 'utf-8')
+  , minimize = require('./lib/minimize');
 
-var content = fs.readFileSync('index.html', 'utf-8');
-
-var handler = new parser.DefaultHandler(function (error, dom) {
-    if (error) return;
-
-    console.log(dom[1].raw)
-}, { ignoreWhitespace: true });
-
-var htmlparser = new parser.Parser(handler);
-htmlparser.parseComplete(content);
-//sys.puts(sys.inspect(handler.dom, false, null));
+minimize(content, function (data) {
+  console.log(data);
+});
